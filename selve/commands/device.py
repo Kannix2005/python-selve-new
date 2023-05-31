96244e9a98885834affe9ba97e364094a0c74f07
Stefan Altheimer <me@stefan-altheimer.de>
2023-05-31 12:22:31 +0200

    Fix device value response

--- selve/commands/device.py
+++ selve/commands/device.py
@@ -102,12 +102,11 @@ class DeviceGetInfoResponse(MethodResponse):
 class DeviceGetValuesResponse(MethodResponse):
     def __init__(self, name, parameters):
         super().__init__(name, parameters)
-        self.name = parameters[0][1] if parameters[0][1] else ""
-        self.movementState = MovementState(int(parameters[2][1])) if int(parameters[2][1]) else MovementState(0)
-        self.value = Util.valueToPercentage(int(parameters[3][1]))
-        self.targetValue = Util.valueToPercentage(int(parameters[4][1]))
+        self.movementState = MovementState(int(parameters[1][1])) if int(parameters[1][1]) else MovementState(0)
+        self.value = Util.valueToPercentage(int(parameters[2][1]))
+        self.targetValue = Util.valueToPercentage(int(parameters[3][1]))
 
-        bArr = Util.intToBoolarray(int(parameters[5][1]))
+        bArr = Util.intToBoolarray(int(parameters[4][1]))
         self.unreachable = bArr[0]
         self.overload = bArr[1]
         self.obstructed = bArr[2]
@@ -119,7 +118,7 @@ class DeviceGetValuesResponse(MethodResponse):
         self.rainAlarm = bArr[8]
         self.freezingAlarm = bArr[9]
 
-        self.dayMode = DayMode(int(parameters[6][1]))
+        self.dayMode = DayMode(int(parameters[5][1]))
 
 
 class DeviceSetFunctionResponse(MethodResponse):
